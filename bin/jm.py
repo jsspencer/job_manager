37f36b3a8fc98f8d99770d7f9d58caa4053a340f
James Spencer <j.spencer@imperial.ac.uk>
2011-09-06 11:07:26 +0100

    document jm.py

--- bin/jm.py
+++ bin/jm.py
@@ -3,31 +3,189 @@
 Synopsis
 --------
 
-jm.py add [-c | --cache] [-s | --server] <job_description>
+.. code-block:: bash
 
-jm.py modify [-c | --cache] [-s | --server] [-i | --index] [-p | --pattern] <job_description>
+    jm.py add [-c | --cache] [-s | --server] <job_description>
 
-jm.py delete [-c | --cache] [-s | --server] [-i | --index] [-p | --pattern]
+    jm.py modify [-c | --cache] [-s | --server] [-i | --index] [-p | --pattern] <job_description>
 
-jm.py list [-c | --cache] [-s | --server] [-p | --pattern]
+    jm.py delete [-c | --cache] [-s | --server] [-i | --index] [-p | --pattern]
 
-jm.py update [-c | --cache]  
+    jm.py list [-c | --cache] [-s | --server] [-p | --pattern]
 
-jm.py daemon [-c | --cache]  
+    jm.py merge [-c | --cache] <[[user@]remote_host:]remote_cache> [remote_hostname]
 
-jm.py merge [-c | --cache] <[[user@]remote_host:]remote_cache> [remote_hostname]
+    jm.py update [-c | --cache]  
+
+    jm.py daemon [-c | --cache]  
 
 Description
 -----------
 
+jm.py provides a command-line interface to the job_manager python module, which
+enables a collection of jobs (principally long-running high-performance
+computing calculations) to be easily monitored and managed.
+
+Jobs can be added, modified, deleted and subsets of the jobs can be viewed.
+Jobs are categorised according to the computer on which they are run.  The
+local computer on which jm.py is run is treated specially and is called
+*localhost*.
+
+Data is saved to a cache file between runs and different cache files can be
+merged together, including caches on remote servers directly over ssh.
+
 Commands
 --------
+
+add
+    Add a job running on the specified server with job details given by job
+    description.
+modify
+    Modify the selected job(s) according to the job description fields
+    supplied.  Note that if neither a pattern nor an index is provided then no
+    job is selected to be modified.
+delete
+    Delete the specified jobs.  Note that if neither a pattern nor an index is
+    provided then no job is selected to be modified.
+list
+    List jobs which match the supplied search criteria.  The complete list of
+    jobs is printed out if no options are specified.  Only fields of the job
+    description which are not null are printed out.
+merge
+    Merge jobs from the remote_cache file into the current cache.
+update
+    Check all jobs on the *localhost* server and update the status of queueing
+    or running jobs if they have started running or finished.  The job status
+    is checked by searching for the *job_id* using ps, qstat (for PBS-based
+    queueing systems) and llq (for LoadLeveler queueing systems).
+daemon
+    Run the update command once a minute.  Designed to be run in the background
+    as a daemon-type process.
+
+Job description
+---------------
+
+The job_description consists of a list of key-value pairs.  A new pair is
+started by a new key, so each value can contain spaces.  The keys must
+terminate with a colon (':') and have a space between the end of the key and
+the first word in the value.  See :ref:`below <examples>` for examples.
+
+Available elements of the job description are:
+
+job_id
+    ID of the job.  This ought to be unique and in order to work with the
+    update and daemon commands should identiy the job by either being the pid
+    of the job (for jobs running interactively) or the ID of the job in the
+    queueing system.  This value is most conveniently obtained from the
+    environment or a queueing system environment variable.
+path
+    Path to the directory in which the job is running.
+program
+    Name of the program being executed.
+input_fname
+    Filename of the input file.
+output_fname
+    Filename of the output file.
+status
+    status of job.  Available values are: unknown, held, queueing, running,
+    finished and analysed.  Default: unknown.
+submit
+    File name of the submit script used.  Only relevant for jobs run on
+    clusters with queueing systems.
+comment
+    Comment and notes on the job.
+
+Unless specified above, all elements default to being a null value.
+
+A job must have a *job_id*, *path* and *program* specified.  Other attributes are optional.  Only the attributes to be set or modified need to specified with the add and modify commands.
 
 Options
 -------
 
+-c, --cache
+    Specify the location of the cache file containing data from previous runs.
+    The default is $HOME/.cache/jm/jm.cache.  The directory structure for the
+    cache file will be created if necessary.
+-s, --server
+    Specify the server of the job.  The default is the *localhost* server
+    except for the **list** command, where the default is all servers.  Can be
+    specified multiple times, in which case the command is applied to each
+    server in turn.  However, this rarely makes sense for the **add** command.
+-i, --index
+    Select a job by its index on the specified server(s).  Can be specified
+    multiple times in order to select multiple jobs.
+-p, --pattern
+    Select a job by a given regular expression on the specified server(s).  The
+    regular expression is tested against all fields in the job description for
+    each job and a job is selected if any of the fields match the regular
+    expression.
+
+.. _examples:
+
 Examples
 --------
+
+Create a job from inside a script.  $$ is the current process id in bash.
+
+.. code-block:: bash
+
+    $ jm.py add job_id: $$ path: $PWD status: running 
+
+List all jobs.
+
+.. code-block:: bash
+
+    $ jm.py list
+
+Modify part of the job description.
+
+.. code-block:: bash
+
+    $ jm.py modify --index 0 comment: a test calculation
+
+Automatically update the status of running jobs
+
+.. code-block:: bash
+
+    $ jm.py update
+
+Run a daemon process to automatically update the status of running jobs once
+a minute using a non-default cache file.
+
+.. code-block:: bash
+
+    $ jm.py daemon --cache /path/to/cache
+
+Merge jobs from a remote server into the local job cache:
+
+.. code-block:: bash
+
+    $ jm.py merge user@remote_server_fqdn:/path/to/remote_cache remote_server
+
+List a subset of jobs.
+
+.. code-block:: bash
+
+    $ jm.py list --server remote_server
+    $ jm.py list --server localhost
+
+Delete a job on the remote server.
+
+.. code-block:: bash
+
+    $ jm.py delete --server remote --index 0
+
+License
+-------
+
+The jm.py script and the job_manager python module are distributed under the
+Modified BSD License.  Please see the source files for more information.
+
+Bugs
+----
+
+Contact James Spencer (james.s.spencer@gmail.com) regarding bug reports,
+suggestions for improvements or code contributions.
 '''
 
 import job_manager
@@ -100,7 +258,7 @@ Values not set default to None.
                      path=None,
                      input_fname=None,
                      output_fname=None,
-                     status=None,
+                     status=job_manager.JobStatus.unknown,
                      submit=None,
                      comment=None,
                    )
@@ -138,11 +296,28 @@ options: an optparse.Values instance containing the set options.
 For full usage, see top-level __doc__.
 '''
 
-    parser = optparse.OptionParser()
+    parser = optparse.OptionParser(usage='''
+%prog add [-c | --cache] [-s | --server] <job_description>
+%prog modify [-c | --cache] [-s | --server] [-i | --index] [-p | --pattern] <job_description>
+%prog delete [-c | --cache] [-s | --server] [-i | --index] [-p | --pattern]
+%prog list [-c | --cache] [-s | --server] [-p | --pattern]
+%prog merge [-c | --cache] <[[user@]remote_host:]remote_cache> [remote_hostname]
+%prog update [-c | --cache]
+%prog daemon [-c | --cache]''',
+                                   description='''Manage and manipulate a set of jobs.
+Options that are not relevant to a command are ignored.  See the man page for
+more details.''',
+                                   epilog='''A job_description consists of
+a series of key: value pairs.  Available keys are job_id, program, path,
+input_fname, output_fname, status, submit and comment.  Allowed status values
+are unknown, held, queueing, running, finished and analysed.  Only job_id,
+program and path are required to add a job and only the attributes to be
+changed are required when modify a job.  Unused attributes are set to null
+values.''')
     parser.add_option('-c', '--cache', default='~/.cache/jm/jm.cache', help='file containing stored job data.  Default: %default.')
-    parser.add_option('-i', '--index', default=[], action='append', type='int', help='index of desired calculation on the server specified by --server.  All jobs are selected if not provided.  Can be specified multiple times to select multiple jobs.')
-    parser.add_option('-s', '--server', default=[], action='append', help='hostname of servers running the calculations.  All servers are selected if not provided.  Can be specified multiple times multiple times to select multiple servers.  Default: localhost.')
-    parser.add_option('-p', '--pattern', help='')
+    parser.add_option('-i', '--index', default=[], action='append', type='int', help='index of desired calculation on the server.  Can be specified multiple times to select multiple jobs.')
+    parser.add_option('-s', '--server', default=[], action='append', help='servers of the job.  Can be specified multiple times to select more than one server.  Default: all servers (list command) or localhost (otherwise).')
+    parser.add_option('-p', '--pattern', help='Select a job by a given regular expression on the specified server(s).')
 
     (options, args) = parser.parse_args(args)
 
